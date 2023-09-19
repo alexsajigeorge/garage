323a2ea4d7475dbd433f92e929dd3e6cfad812be
alexsajigeorge <alexsajigeorge@gmail.com>
2023-09-19 23:43:11 +0400

    search bar path query update

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
 
         {!isDataEmpty ? (
           <section>
-            <div className="home__cars-wraper">
+            <div className="home__cars-wrapper">
               {allCars?.map((car) => (
                 <CarCard car={car} />
               ))}
