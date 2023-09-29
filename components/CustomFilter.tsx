0b06e62d430ef43908b737bd1f3797240c89e53a
alexsajigeorge <alexsajigeorge@gmail.com>
2023-09-29 13:19:37 +0400

    custom filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useState,Fragment } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { Listbox, Transition } from '@headlessui/react';
 
 const CustomFilter = () => {
   return (
