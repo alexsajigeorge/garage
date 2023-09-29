765ee98f2b279411b1bd0bcef1c0d2d5c67a5989
alexsajigeorge <alexsajigeorge@gmail.com>
2023-09-29 14:19:51 +0400

    type error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { fetchCars } from "@/utils";
-import { HomeProps } from "@types";
+import { HomeProps } from "@/types";
 import { fuels, yearsOfProduction } from "@/constants";
 import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@/components";
 
